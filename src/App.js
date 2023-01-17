cc507c3e0c7c20ad6f39b431aec84e09a2362df8
kronnor <kevin.landivar.est@tecazuay.edu.ec>
2023-01-16 19:50:16 -0500

    dos

--- src/App.js
+++ src/App.js
@@ -2,41 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [archivos, setArchivos] = useState(null)
-  const subirArchivos = e => {
-    setArchivos(e);
-    console.log(e);
-
-  }
-  const insertarArchivos = async () => {
-
-    const f = new FormData();
-
-    for (let index = 0; index < archivos.length; index++) {
-      f.append("file", archivos[index]);
-
-    }
-
-    console.log("Aqui " + f)
-    await axios.post("http://localhost:8080/api/assets/upload", f, { headers: { 'Content-Type': 'multipart/form-data' } })
-
-      .then(response => {
-        console.log(response.data);
-        console.log(f)
-      }).catch(error => {
-        console.log(error);
-      })
-  }
   return (
     <div className="App">
-
+       <header>
+        <nav>
+            <a href="#">INICIO </a>
+            <a href="#">ACERCA DE MI </a>
+            <a href="#">CONTACTOS </a>
+        </nav>
+    </header>
+    <title style="background-color: lavender;">REACT</title>
       <label>cargar foto</label>
       <input type= "file"></input>
-      <input type="file" id="subir" name="PDF" onChange={(e) => subirArchivos(e.target.files)} />
-      <button onClick={() => insertarArchivos()}>subir foto</button>
     </div>
   );
 }
-
-
 export default App;
